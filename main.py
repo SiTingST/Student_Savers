f090e80bcc6716da818e7667131400fe8085dfa1
SiTingST <33442908+SiTingST@users.noreply.github.com>
2020-07-26 21:12:25 +0800

    one

--- main.py
+++ main.py
@@ -1247,7 +1247,6 @@ def main():
     # log all errors
     dp.add_error_handler(error)
     # Start the Bot
-    updater.start_polling()
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
                           url_path=TOKEN)
