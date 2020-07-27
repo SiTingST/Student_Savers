14f4426215a7a23b07894b40f9e73bece4396b85
SiTingST <33442908+SiTingST@users.noreply.github.com>
2020-07-27 10:58:53 +0800

    one

--- main3.py
+++ main3.py
@@ -1251,8 +1251,10 @@ def main():
     # log all errors
     dp.add_error_handler(error)
     # Start the Bot
-    
-
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://student-saversbot.herokuapp.com/' + TOKEN)
 
 if __name__ == '__main__':
     main()
